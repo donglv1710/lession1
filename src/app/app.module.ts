2f0eb4a3c32776d231be75eb7433c8b50fdac085
donglv1710 <fdkvnvn@gmail.com>
2022-11-18 15:50:40 +0700

    binding data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
